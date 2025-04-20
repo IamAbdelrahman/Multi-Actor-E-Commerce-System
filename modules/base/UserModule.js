09c8148a088cc175e9aab32a77f3e30908a3c8c5
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-04-20 13:53:29 +0200

    UserModule

---
+++ modules/base/UserModule.js
@@ -0,0 +1,90 @@
+import {StorageManager} from './StorageModule.js'
+
+class User {
+  constructor(id, name, email) {
+    this.ID = id;
+    this.Name = name;
+    this.Email = email;
+  }
+
+  set ID(id) {
+    if (typeof id === 'number' && id > 0) {
+      this.id = id;
+    } else {
+      console.error("Invalid ID: must be a positive number.");
+      this.id = 0;
+    }
+  }
+
+  get ID() {
+    return this.id;
+  }
+
+  set Name(value) {
+    if (User.isNameValid(value)) {
+      this.name = value.trim();
+    } else {
+      console.error("Invalid name: must be a string with at least 3 characters.");
+      this.name = null;
+    }
+  }
+
+  get Name() {
+    return this.name;
+  }
+
+  set Email(value) {
+    if (User.isEmailValid(value)) {
+      this.email = value.toLowerCase();
+    } else {
+      console.error("Invalid email format.");
+      this.email = null;
+    }
+  }
+
+  get Email() {
+    return this.email;
+  }
+
+  static isNameValid(value) {
+    return value.match(/^[a-zA-Z]{3,9}$/);
+  }
+
+  static isEmailValid(value) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return value.match(emailPattern);
+  }
+}
+
+
+export default class UserManager {
+  static CreateUser(id, name, email) {
+    const user = new User(id, name, email);
+    if (user.name == null || user.email == null || user.id == 0) {
+      console.error("Please, re-enter a valid data!");
+      return ;
+    }
+    const data = StorageManager.Load("users");
+    const users = data?.users || [];
+    users.push(user);
+    StorageManager.Save("users", users);
+  }
+
+  static GetUser(id) {
+    const users = StorageManager.Load("users") || [];
+    return users.find(user => user.id === id);
+  }
+
+  static UpdateUser(id, name, email) {
+    var data = StorageManager.Load("users");
+    var users = data?.users || [];
+    users = users.map(user => user.id === id ? new user(id, name, email) : user);
+  }
+
+  static DeleteUser(id) {
+    const data = StorageManager.Load("users");
+    var users = data?.users || [];
+    users = users.filter(user => user.id !== id);
+    StorageManager.Save("users", users);
+  }
+}
