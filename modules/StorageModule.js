30da3fde616d0c0729f24d6e6bf975d320fae243
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-04-24 11:18:08 +0200

    Storage Module

---
+++ modules/StorageModule.js
@@ -0,0 +1,25 @@
+export default class StorageManager {
+  static SaveSection(section, value) {
+    const data = StorageManager.Load("data") || {};
+    data[section] = value;
+    StorageManager.Save("data", data);
+  }
+
+  static LoadSection(section) {
+    const data = StorageManager.Load("data") || {};
+    return data[section] || [];
+  }
+
+  static Save(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+
+  static Load(key) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+  }
+
+  static Remove(key) {
+    localStorage.removeItem(key);
+  }
+}
