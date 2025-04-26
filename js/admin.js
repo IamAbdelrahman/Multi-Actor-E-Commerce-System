c74c92553542b1314e2de4fac2395a409970ef93
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-04-26 23:23:57 +0300

    Add Charts

--- js/admin.js
+++ js/admin.js
@@ -35,6 +35,30 @@ toggleBtn.addEventListener("click", function () {
   
 });
 
+new Chart(document.getElementById("bar-chart-grouped"), {
+  type: 'bar',
+  data: {
+    labels: ["1900", "1950", "1999", "2050"],
+    datasets: [
+      {
+        label: "Africa",
+        backgroundColor: "#3e95cd",
+        data: [133,221,783,2478]
+      }, {
+        label: "Europe",
+        backgroundColor: "#8e5ea2",
+        data: [408,547,675,734]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Population growth (millions)'
+    }
+  }
+});
+
 /*- HELPER FUNCTIONS
 -----------------------------------------------------------------------*/
 // function createRow(id, name, email, password, role, city, phone) {
