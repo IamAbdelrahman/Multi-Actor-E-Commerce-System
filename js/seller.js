f44a93d5b22ad841e0e25b4791a1cd11d234b97c
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-04-28 16:52:14 +0300

    Seller Script

--- js/seller.js
+++ js/seller.js
@@ -42,3 +42,15 @@ new Chart(document.getElementById("bar-chart-grouped"), {
     }
   }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  ShowUsers();
+
+  const toggleBtn = document.querySelector(".toggle-btn");
+  const toggler = document.querySelector("#icon");
+  toggleBtn.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("expand");
+  toggler.classList.toggle("bxs-chevrons-right");
+  toggler.classList.toggle("bxs-chevrons-left");
+  });
