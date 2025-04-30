17e6f04cbdee81160d6715ac9c01125ffd0aff77
Hagar-yusuf306 <hagaryusuf1@gmail.com>
2025-04-30 11:10:38 +0300

    commitn

--- js/popUp.js
+++ js/popUp.js
@@ -1,22 +1,10 @@
-//PopUp JS
-document.addEventListener("DOMContentLoaded", () => {
-    // Load modal from external file
-    fetch('#')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('modalContainer').innerHTML = html;
+const modal = document.getElementById("Thing");
+const icon = document.getElementById("Thing-Icon");
+const closeBtn = document.getElementById("closepopup");
 
-            const modal = document.getElementById("Thing");
-            const icon = document.getElementById("Thing-Icon");
-            const closeBtn = document.getElementById("closepopup");
 
-            if (icon && modal && closeBtn) {
-                icon.onclick = () => modal.classList.remove('d-none');
-                closeBtn.onclick = () => modal.classList.add('d-none');
-                window.onclick = (e) => {
-                    if (e.target === modal) modal.classList.add('d-none');
-                };
-            }
-        })
-});
-
+icon.onclick = () => modal.classList.remove('d-none');
+closeBtn.onclick = () => modal.classList.add('d-none');
+window.onclick = (e) => {
+    if (e.target === modal) modal.classList.add('d-none');
+}
