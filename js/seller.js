ded9f462c6d8778ebbaa239f3e26276a603f9a4f
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-05-14 10:17:04 +0300

    Updates

--- js/seller.js
+++ js/seller.js
@@ -158,10 +158,9 @@ function ShowProducts() {
 
 function CreateOrdersHeader() {
   AssignHeader("Manage Orders");
-  var modal = CreateOrderModal("Orders", "Approve", "Reject");
   const table = createTable();
   const contentdiv = document.querySelector("#mainContent");
-  contentdiv.innerHTML = modal + table;
+  contentdiv.innerHTML =  table;
 
   const head = document.querySelector("thead");
   const tr = document.createElement("tr");
@@ -592,9 +591,7 @@ function createTable() {
     <div class="container-fluid px-4 mt-4">
       <div class="row">
         <div class="col-12">
-          <div class="d-flex justify-content-between align-items-center mb-3">
-            <input type="text" class="form-control w-25" placeholder="Search By Id..." id="searchInput">
-          </div>
+
 
           <div class="table-responsive shadow-sm rounded bg-white p-2">
             <table class="table table-striped table-bordered table-hover align-middle text-center">
@@ -680,43 +677,35 @@ function CreateModal(type, ...actions) {
   return modal;
 }
 
-function CreateOrderModal(type, ...actions) {
-  var modal = `
-    <div class="my-4">
-      <button class="btn btn-danger me-2" data-bs-toggle="modal" data-bs-target="#${type}ActionModal" data-action="${actions[0]}">
-        ${actions[0]} ${type}
-      </button>
-      <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#${type}ActionModal" data-action="${actions[1]}">
-        ${actions[1]} ${type}
-      </button>
-    </div>
+// function CreateOrderModal(type, ...actions) {
+//   var modal = `
 
-      <div class="modal fade" id="${type}ActionModal" tabindex="-1" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="modalTitle">${actions[0]}/${actions[1]} ${type}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
+//       <div class="modal fade" id="${type}ActionModal" tabindex="-1" aria-hidden="true">
+//       <div class="modal-dialog">
+//         <div class="modal-content">
+//           <div class="modal-header">
+//             <h5 class="modal-title" id="modalTitle">${actions[0]}/${actions[1]} ${type}</h5>
+//             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//           </div>
 
-          <div class="modal-body">
-            <form id="${type}ActionForm">
-              <div class="mb-3">
-                <label for="${type}Id" class="form-label">Enter ${type} ID</label>
-                <input type="number" class="form-control" id="${type}Id" required>
-              </div>
-            </form>
-          </div>
+//           <div class="modal-body">
+//             <form id="${type}ActionForm">
+//               <div class="mb-3">
+//                 <label for="${type}Id" class="form-label">Enter ${type} ID</label>
+//                 <input type="number" class="form-control" id="${type}Id" required>
+//               </div>
+//             </form>
+//           </div>
 
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-            <button type="button" class="btn btn-primary" id="confirmAction">Confirm</button>
-          </div>
-        </div>
-      </div>
-    </div>`
-  return modal;
-}
+//           <div class="modal-footer">
+//             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+//             <button type="button" class="btn btn-primary" id="confirmAction">Confirm</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>`
+//   return modal;
+// }
 
 function ClearForm(type) {
   document.getElementById(`${type}Name`).value = "";
